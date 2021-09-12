35ebbc76b22aa11c8a9f449770a0f3935e66bfbd
Maxime Pujol <maxime.pujol@outlook.com>
2021-09-12 18:21:54 +0200

    typo config protocol

--- src/js/Config.js
+++ src/js/Config.js
@@ -1,10 +1,10 @@
 class Config {
     constructor() {
-        this.serverprotocol = process.env.YTDL_SERVER_PROCOTOL || 'http';
+        this.serverprotocol = process.env.YTDL_SERVER_PROTOCOL || 'http';
         this.serverhost = process.env.YTDL_SERVER_HOST || 'localhost';
         this.serverport = process.env.YTDL_SERVER_PORT || '3000';
 
-        this.mainprotocol = process.env.YTDL_MAIN_PROCOTOL || 'http';
+        this.mainprotocol = process.env.YTDL_MAIN_PROTOCOL || 'http';
         this.mainhost = process.env.YTDL_MAIN_HOST || 'localhost';
         this.mainport = process.env.YTDL_MAIN_PORT || '3000';
 
